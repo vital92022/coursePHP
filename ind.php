4581b101500496e17f638d7c97da1d8f4d5f2126
vital92022 <vetali92015@mail.ru>
2022-09-01 14:24:18 +0300

    The first part

---
+++ ind.php
@@ -0,0 +1,16 @@
+<?php
+
+$myArray = [154354325,3,"kykyky",4.4,7];
+$mySecondArray = array();
+
+var_dump($myArray);
+var_dump($mySecondArray);
+
+$customArray = [
+	15 => 5,
+	11 => "321312"
+];
+var_dump($customArray);
+print_r($customArray);
+
+?>
